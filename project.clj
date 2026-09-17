a15f265cead73ce0a550acc6523b6f62e4486d32
Walter Pelissero <walter@pelissero.de>
2026-09-17 15:44:30 +0200

    Add qrcode and update dependencies

--- project.clj
+++ project.clj
@@ -17,42 +17,32 @@
                  [org.clojure/tools.logging "1.3.1"]
                  ;; WARNING: Version 2.x breaks compatibility
                  [reagent "1.3.0"]
-                 [thheller/shadow-cljs "3.5.1"]
+                 [thheller/shadow-cljs "3.5.3"]
                  [org.eclipse.paho/org.eclipse.paho.client.mqttv3 "1.2.5"]
                  ;; Tailwind CLI packed as a runnable Java jar dependency
                  [org.webjars.npm/tailwindcss "4.3.3"]
                  [org.clojure/clojurescript "1.12.145"]
                  [ring-transit "0.1.6"]
                  [com.cognitect/transit-cljs "0.8.280"]
-                 [com.github.oshi/oshi-core "6.6.5"]
-                 #_[org.slf4j/slf4j-simple "2.0.19"]]
+                 [com.github.oshi/oshi-core "7.6.1"]
+                 [com.google.zxing/core "3.5.4"]
+                 [com.google.zxing/javase "3.5.4"]
+                 [cljsjs/qrcode-generator "1.4.4-0"]]
 
-  :plugins [#_[lein-cljsbuild "1.1.8"]
-            [lein-shell "0.5.0"]]
+  ;; WARNING: do not include the lein-cljsbuild plugin!  This project
+  ;; uses shadow-cljs instead!
+  :plugins [[lein-shell "0.5.0"]]
 
   :source-paths ["src/clj" "src/cljs"]
   :resource-paths ["resources"]
   :main ^:skip-aot fourteatoo.httpad.core
-  :aliases {"build-cljs" ["shell" "npx" "shadow-cljs" "release" "client"]}
+  :aliases {"npm-install"   ["shell" "npm" "install"]
+            "build-cljs"    ["shell" "npx" "shadow-cljs" "release" "client"]
+            "ancient-all"   ["do" ["ancient"] ["shell" "npx" "npm-check-updates"]]
+            ;; Single entry-point for uberjar packaging:
+            ;; Runs npm install -> shadow-cljs release -> standard lein uberjar
+            "package"       ["do" ["npm-install"] ["build-cljs"] ["uberjar"]]}
   :profiles {:uberjar {:aot :all
-                       :prep-tasks ["compile" ["shell" "npx" "shadow-cljs" "release" "client"]]
                        :jvm-opts ["-Xmx2g"
                                   "-Dclojure.compiler.direct-linking=true"
-                                  "-Djdk.attach.allowAttachSelf"]}}
-
-  #_#_:cljsbuild {:builds
-              [{:id "dev"
-                :source-paths ["src/cljs"]
-                :compiler {:main fourteatoo.httpad.client
-                           :output-to "resources/public/js/main.js"
-                           :output-dir "resources/public/js/out"
-                           :optimizations :none
-                           :source-map true}}
-               {:id "min"
-                :source-paths ["src/cljs"]
-                :compiler {:main fourteatoo.httpad.client
-                           :output-to "resources/public/js/main.js"
-                           :output-dir "resources/public/js/out"
-                           :asset-path "/js/out"
-                           :optimizations :advanced
-                           :pretty-print false}}]})
+                                  "-Djdk.attach.allowAttachSelf"]}})
