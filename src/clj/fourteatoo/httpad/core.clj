571937efe861559961c5333ebc8be3b0d04f4d6f
Walter Pelissero <walter@pelissero.de>
2026-09-17 16:15:10 +0200

    Print QR on terminal

--- src/clj/fourteatoo/httpad/core.clj
+++ src/clj/fourteatoo/httpad/core.clj
@@ -9,7 +9,8 @@
    [fourteatoo.httpad.server :as server]
    [fourteatoo.httpad.mqtt :as mqtt]
    [mount.core :as mount :refer [defstate]]
-   [fourteatoo.httpad.rt :as rt]))
+   [fourteatoo.httpad.rt :as rt]
+   [fourteatoo.httpad.qr :as qr]))
 
 
 (def cli-options
@@ -23,12 +24,12 @@
    ["-h" "--help" "Show this"]])
 
 (defn print-ui-urls [port]
-  (let [local-url (str "http://localhost:" port "/index.html")
-        lan-ip    (network/get-ip-address)
-        lan-url   (str "http://" lan-ip ":" port "/index.html")]
-    (println "UI is running on port" port)
-    (println (str "Local URL: " local-url))
-    (println (str "LAN URL:   " lan-url))))
+  (let [make-url (fn [host]
+                   (str "http://" host ":" port "/index.html"))
+        lan-url (make-url (network/get-ip-address))]
+    (qr/print-small-qr lan-url)
+    (println (str "URL: " lan-url))
+    (println (str "  or " (make-url "localhost")))))
 
 (defn usage [summary errors]
   (run! println errors)
@@ -37,10 +38,10 @@
   (System/exit 1))
 
 ;; NOTE: if namespaces are completely self-contained, we may miss to
-;; include them, and even if we do require them, cljr may later remove
+;; require them, and even if we do require them, cljr may later remove
 ;; the dependency. As a consequence, mount will miss the dependency
-;; and skip to start the states defined in those namespace.  The
-;; solution is either use at least a function defined in the other
+;; and skip to start the states defined in those namespaces.  The
+;; solution is either to use at least a function defined in the other
 ;; namespace, or move the state here altogether.  Thus the seemingly
 ;; useless logs below.
 
@@ -50,7 +51,7 @@
     (rt/arm-exit-hooks)
     (log/info (mount/start))
     (print-ui-urls (c/port))
-    (println "HTTPAD running\ntype C-c to stop the program")
+    (println "\nHTTPAD running\ntype C-c to stop it")
     (when (opt :launch-ui)
       (browser/open-browser (str "http://localhost:" (c/port) "/index.html")))
     ;; WARNING: don't remove these logs!
--- src/clj/fourteatoo/httpad/qr.clj
+++ src/clj/fourteatoo/httpad/qr.clj
@@ -3,11 +3,36 @@
            [com.google.zxing.qrcode QRCodeWriter]
            [com.google.zxing.client.j2se MatrixToImageWriter]))
 
-(defn print-terminal-qr [url]
+
+(defn print-large-qr [url]
   (let [writer (QRCodeWriter.)
         matrix (.encode writer url BarcodeFormat/QR_CODE 25 25)]
-    (println "\nScan with mobile device:")
     (dotimes [y (.getHeight matrix)]
       (dotimes [x (.getWidth matrix)]
         (print (if (.get matrix x y) "██" "  ")))
       (println))))
+
+(comment
+  (print-large-qr "http://10.0.0.111:8080/index.html"))
+
+
+(defn print-small-qr [url]
+  (let [writer (QRCodeWriter.)
+        matrix (.encode writer url BarcodeFormat/QR_CODE 25 25)
+        w      (.getWidth matrix)
+        h      (.getHeight matrix)]
+    ;; Process two vertical rows at a time
+    (doseq [y (range 0 h 2)]
+      (doseq [x (range w)]
+        (let [top    (.get matrix x y)
+              bottom (if (< (inc y) h) (.get matrix x (inc y)) false)]
+          (print (cond
+                   (and top bottom) "█" ; Both filled
+                   top              "▀" ; Top filled, bottom empty
+                   bottom           "▄" ; Top empty, bottom filled
+                   :else            " ")))) ; Both empty
+      (println))))
+
+(comment
+  (print-compact-qr "http://10.0.0.111:8080/index.html"))
+
