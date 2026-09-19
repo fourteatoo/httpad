f1f9c9ed91abb118419b7f5c3c4239ea20033366
Walter Pelissero <walter@pelissero.de>
2026-09-19 22:13:01 +0200

    Make sure QR code is readable on any terminal configuration

--- src/clj/fourteatoo/httpad/qr.clj
+++ src/clj/fourteatoo/httpad/qr.clj
@@ -1,15 +1,14 @@
 (ns fourteatoo.httpad.qr
   (:import [com.google.zxing BarcodeFormat]
-           [com.google.zxing.qrcode QRCodeWriter]
-           [com.google.zxing.client.j2se MatrixToImageWriter]))
+           [com.google.zxing.qrcode QRCodeWriter]))
 
 
 (defn print-large-qr [url]
   (let [writer (QRCodeWriter.)
-        matrix (.encode writer url BarcodeFormat/QR_CODE 25 25)]
+        matrix (.encode writer url BarcodeFormat/QR_CODE 0 0)]
     (dotimes [y (.getHeight matrix)]
       (dotimes [x (.getWidth matrix)]
-        (print (if (.get matrix x y) "██" "  ")))
+        (print (if (.get matrix x y) "  " "██")))
       (println))))
 
 (comment
@@ -18,7 +17,7 @@
 
 (defn print-small-qr [url]
   (let [writer (QRCodeWriter.)
-        matrix (.encode writer url BarcodeFormat/QR_CODE 25 25)
+        matrix (.encode writer url BarcodeFormat/QR_CODE 0 0)
         w      (.getWidth matrix)
         h      (.getHeight matrix)]
     ;; Process two vertical rows at a time
@@ -27,12 +26,11 @@
         (let [top    (.get matrix x y)
               bottom (if (< (inc y) h) (.get matrix x (inc y)) false)]
           (print (cond
-                   (and top bottom) "█" ; Both filled
-                   top              "▀" ; Top filled, bottom empty
-                   bottom           "▄" ; Top empty, bottom filled
-                   :else            " ")))) ; Both empty
+                   (and top bottom) " " ; Both dark modules -> empty space
+                   top              "▄" ; Top dark -> bottom half-block
+                   bottom           "▀" ; Bottom dark -> top half-block
+                   :else            "█")))) ; Both empty
       (println))))
 
 (comment
-  (print-compact-qr "http://10.0.0.111:8080/index.html"))
-
+  (print-small-qr "http://10.0.0.111:8080/index.html"))
